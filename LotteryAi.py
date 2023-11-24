92e62264ca9e5f7b8873c248eebddfc7de6ec44d
alld0n3 <aldo.panetta@gmail.com>
2023-11-24 12:59:01 +0100

    x4me
    x4me

--- LotteryAi.py
+++ LotteryAi.py
@@ -26,7 +26,7 @@ def print_intro():
 # Function to load data from a file and preprocess it
 def load_data():
     # Load data from file, ignoring white spaces and accepting unlimited length numbers
-    data = np.genfromtxt('data.txt', delimiter=',', dtype=int)
+    data = np.genfromtxt('data.csv', delimiter=',', dtype=int)
     # Replace all -1 values with 0
     data[data == -1] = 0
     # Split data into training and validation sets
